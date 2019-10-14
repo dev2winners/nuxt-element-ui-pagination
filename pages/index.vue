6b1b65c23e0b0c903407af2ac4e7cdfdd4cb2b3c
Merecoin <36365036+merecoin@users.noreply.github.com>
2019-10-14 22:27:32 +0300

    w/o pagination

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="container">{{ films }}</div>
+  <div class="container">
+    <div v-for="film in films" :key="film.id" class="films">
+      <el-card :body-style="{ padding: '0px' }">
+        <img
+          src="https://www.downloadwallpapers.info/preview/2015/11/10/810396_overallsite-ps3_1920x1080_h.jpg"
+          class="image"
+        />
+        <div style="padding: 14px;">
+          <span class="name">{{ film.name }}</span>
+          <span class="actors">{{ film.actors }}</span>
+          <span class="genres">{{ film.genres }}</span>
+          <span class="description">{{ film.description }}</span>
+        </div>
+      </el-card>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,13 +29,48 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 .container {
+  font-family: "Montserrat", "Verdana", "Arial";
   margin: 0 auto;
   min-height: 100vh;
-  display: flex;
+  // display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  text-align: left;
+  .films {
+    max-width: 400px;
+    margin-bottom: 20px;
+  }
+  span {
+    display: block;
+  }
+  .name {
+    font-size: 1.3em;
+    font-weight: 500;
+    color: #777;
+    padding-bottom: 5px;
+  }
+  .actors {
+    font-size: 0.9em;
+    font-weight: 400;
+    color: #444;
+    padding-bottom: 5px;
+  }
+  .genres {
+    font-size: 0.7em;
+    font-weight: 600;
+    color: #444;
+    padding-bottom: 5px;
+  }
+  .description {
+    font-size: 0.9em;
+    font-weight: 400;
+    color: #444;
+    line-height: 1.63;
+  }
+  .image {
+    width: 100%;
+  }
 }
 </style>
